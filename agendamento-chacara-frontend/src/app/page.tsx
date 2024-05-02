8d2e2e985ef00336d59de71e0461fc91f47dd25c
Luisfelippedev <felippe.luisdev@gmail.com>
2024-05-02 17:17:32 -0300

    feat: create and styling components and page of reservations

--- agendamento-chacara-frontend/src/app/page.tsx
+++ agendamento-chacara-frontend/src/app/page.tsx
@@ -1,12 +1,31 @@
+"use client";
 import { HomePage } from "@/components/Home/Home";
 import styles from "./page.module.scss";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+});
 
 export default function Home() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <main className={styles.main}>
         <HomePage />
       </main>
-    </>
+    </ThemeProvider>
   );
 }
